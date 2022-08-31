b96b56adc66bb1b1195ed05d40a50190f89c8a74
catehuang <s0350228@gmail.com>
2022-08-31 10:03:51 -0600

    change to detect data file for high score

--- scoreboard.py
+++ scoreboard.py
@@ -1,8 +1,9 @@
 from turtle import Turtle
-
+from pathlib import Path
 
 ALIGNMENT = "center"
 FONT = ("Arial", 20, "normal")
+DATA_FILE = "data.txt"
 
 
 class Scoreboard(Turtle):
@@ -10,8 +11,12 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        with open("data.txt") as data:
-            self.high_score = int(data.read())
+        path = Path(DATA_FILE)
+        if path.is_file():
+            with open("data.txt") as data:
+                self.high_score = int(data.read())
+        else:
+            self.high_score = 0
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -20,7 +25,7 @@ class Scoreboard(Turtle):
 
     def update_scoreboard(self):
         self.clear()
-        self.write(f"Score: {self.score} High score: {self.high_score}", align=ALIGNMENT, font=FONT)
+        self.write(f" High score: {self.high_score}\tScore: {self.score}", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
